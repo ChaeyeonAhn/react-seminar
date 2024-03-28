c391e51e1b7da4bd1497ad9328cdec968efb8667
chacha <chacha@sparcs.org>
2024-03-29 01:46:16 +0900

    done?

--- src/app/Todo.jsx
+++ src/app/Todo.jsx
@@ -2,23 +2,12 @@ import { useState } from "react";
 import { useRef } from "react";
 import { useEffect } from "react";
 
-// 제출 버튼 누르면 로컬 스토리지에 해당 텍스트 받아서 저장.
-// 돔 객체에 접근, useRef
-// 그리고 계속 제출한 건 사용자에게 보이게 띄워 놓아야 함.
-// useState
-// 수정 버튼 누르면 ??? 해당 항목의 키를 찾아 그 value 값을 수정할 수 있도록 해야 함.
-// 삭제 버튼 누르면 로컬 스토리지에서 해당 키 찾아 삭제.
-
 // ---------------------------------------------------------------------------------------------
 // LocalStorage 도 react 외적인 거니까 useEffect 써서 아규먼트로 함수와 빈 어레이를 넣어주는 과정이 필요하고,
 // 어레이가 바뀔 때마다 함수가 실행되는데, state (화면에 띄워지는 리스트)가 바뀌어서 띄워진다..!
 
 // 로컬 스토리지에 저장할 때도 키 계속 여러개 하지 말고, 키 하나에 아예 어레이를 value 로 넣는 식으로 해서 해보잣
 // ---------------------------------------------------------------------------------------------
-
-// 새로고침 해도 로컬 스토리지에 있는 것들 다 보여주려면
-// 로컬 스토리지에 뭐가 있다면 그걸 다 보여주기
-// 없다면 뭐 써서 입력하면 그 때부터 어레이 생겨서 그거 보여주기
 
 export const Todo = () => {
   const [todo, setToDo] = useState(JSON.parse(window.localStorage.getItem("toDoList")));
@@ -38,11 +27,6 @@ export const Todo = () => {
     }
   }, []);
 
-  // useEffect(() => {
-  //   setToDo(JSON.parse(window.localStorage.getItem('toDoList')));
-
-  // }, []);
-
   const Display = () => {
     var displayToDo = "";
 
@@ -51,8 +35,7 @@ export const Todo = () => {
       displayToDo = todo.map((el) => (
         <li className="toDoList" key={el.id}>
           <div id="left">
-            <input
-              type="checkbox"
+            {/* <input type = "checkbox"
               onChange={() => {
                 const array = JSON.parse(window.localStorage.getItem("toDoList"));
                 for (let i = 0; i < array.length; i++) {
@@ -64,11 +47,32 @@ export const Todo = () => {
                 window.localStorage.setItem("toDoList", JSON.stringify(array));
                 setToDo(array);
               }}
-            ></input>
+            ></input> */}
+
+            <button
+              id="checkbox"
+              style={{ backgroundColor: el.color }}
+              onClick={() => {
+                const array = JSON.parse(window.localStorage.getItem("toDoList"));
+                for (let i = 0; i < array.length; i++) {
+                  if (array[i].id === el.id) {
+                    if (array[i].done) {
+                      array[i].done = false;
+                      array[i].color = "red";
+                    } else {
+                      array[i].done = true;
+                      array[i].color = "blue";
+                    }
+                  }
+                }
+                window.localStorage.setItem("toDoList", JSON.stringify(array));
+                setToDo(array);
+              }}
+            ></button>
 
             <h4 id="text">
               {el.modify ? (
-                <input width="90%" ref={modifiedInput} defaultValue={el.text}></input>
+                <input width="90%" size="30" ref={modifiedInput} defaultValue={el.text}></input>
               ) : el.done ? (
                 <del>{el.text}</del>
               ) : (
@@ -161,11 +165,13 @@ export const Todo = () => {
                   text: userInput.current.value,
                   modify: false,
                   done: false,
+                  color: "red",
                 }); // 받은 텍스트를 배열 안에 넣어줌
 
                 const finalArray = JSON.stringify(array2); // 다시 배열을 문자로 패킹해서
                 window.localStorage.setItem("toDoList", finalArray); // 로컬 스토리지에 넣어주기
                 setToDo(array2);
+                userInput.current.value = "";
               }
 
               console.log(todo);
@@ -184,11 +190,13 @@ export const Todo = () => {
                   text: userInput.current.value,
                   modify: false,
                   done: false,
+                  color: "red",
                 }); // 받은 텍스트를 배열 안에 넣어줌
 
                 const finalArray = JSON.stringify(array2); // 다시 배열을 문자로 패킹해서
                 window.localStorage.setItem("toDoList", finalArray); // 로컬 스토리지에 넣어주기
                 setToDo(array2);
+                userInput.current.value = "";
               }
             }
 
